d0bba6115f96dabe6f62bdd9631cd5fd246ce0c9
Funwayguy <FunwayguyEmail@gmail.com>
2014-03-14 15:09:19 +1000

    Finished 'The Surprise'

--- enviromine/handlers/SurpriseHandler.java
+++ enviromine/handlers/SurpriseHandler.java
@@ -4,13 +4,23 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Random;
 import java.util.logging.Level;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntitySpider;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ChatMessageComponent;
 import enviromine.EntityPhysicsBlock;
 import enviromine.core.EnviroMine;
+import enviromine.trackers.EnviroDataTracker;
 
 public class SurpriseHandler
 {
@@ -21,7 +31,7 @@ public class SurpriseHandler
 	{
 		surpriseRate += 1;
 		
-		if(surpriseRate > 60)
+		if(surpriseRate > 120)
 		{
 			SurpriseHandler.initSurprise();
 			surpriseRate = 0;
@@ -59,11 +69,6 @@ public class SurpriseHandler
 				return;
 			}
 			
-			if(!message.equals(""))
-			{
-				target.sendChatToPlayer(ChatMessageComponent.createFromText(message));
-			}
-			
 			int x = (int)Math.floor(target.posX);
 			int y = (int)Math.floor(target.posY);
 			int z = (int)Math.floor(target.posZ);
@@ -99,7 +104,7 @@ public class SurpriseHandler
 						Minecraft.getMinecraft().thePlayer.sendChatMessage(data[4]);
 					}
 				}
-			} else if(actionType.equalsIgnoreCase("Message") && data.length > 4)
+			} else if(actionType.equalsIgnoreCase("Message") && data.length >= 4)
 			{
 				if(target.worldObj.isRemote)
 				{
@@ -133,20 +138,148 @@ public class SurpriseHandler
 					return;
 				}
 				
-				System.out.println("EMS ID: " + actionType + " at (" + x + "," + y + "," + z + ")");
-				
 				try
 				{
-					float volume = Float.parseFloat(data[5]);
-					float pitch = Float.parseFloat(data[6]);
-					target.worldObj.playSoundAtEntity(target, data[4], volume, pitch);
+					float volume = Float.parseFloat(data[5].trim());
+					float pitch = Float.parseFloat(data[6].trim());
+					target.worldObj.playSoundAtEntity(target, data[4].trim(), volume, pitch);
+					
+					System.out.println("EMS ID: " + actionType + " Args (" + data[4].trim()  + ", " + volume + ", " + pitch + ")");
 				} catch(NumberFormatException e)
 				{
 					return;
 				}
-			} else if(actionType.equalsIgnoreCase("CreeperJockey"))
+			} else if(actionType.equalsIgnoreCase("CreeperJockey") && data.length == 4)
 			{
+				if(target.worldObj.isRemote)
+				{
+					return;
+				}
 				
+				Random rand = target.getRNG();
+				
+				int i = (int)(x + rand.nextInt(20) - 10);
+				int j = (int)(y + rand.nextInt(2) - 1);
+				int k = (int)(z + rand.nextInt(20) - 10);
+				
+				if(Math.abs(x - i) < 4 || Math.abs(z - k) < 4)
+				{
+					return;
+				}
+				
+				EntityCreeper creeper = new EntityCreeper(target.worldObj);
+				EntitySpider spider = new EntitySpider(target.worldObj);
+				
+				spider.setPositionAndRotation(i, j, k, rand.nextFloat() * 360F, 0.0F);
+				creeper.setPositionAndRotation(i, j, k, rand.nextFloat() * 360F, 0.0F);
+				
+				if(spider.getCanSpawnHere())
+				{
+					target.worldObj.spawnEntityInWorld(spider);
+					target.worldObj.spawnEntityInWorld(creeper);
+					creeper.mountEntity(spider);
+			        creeper.getDataWatcher().updateObject(17, Byte.valueOf((byte)1));
+			        spider.getEntityAttribute(SharedMonsterAttributes.followRange).setAttribute(64D);
+			        creeper.getEntityAttribute(SharedMonsterAttributes.followRange).setAttribute(64D);
+					
+					System.out.println("EMS ID: " + actionType + " at (" + i + "," + j + "," + k + ")");
+				} else
+				{
+					return;
+				}
+			} else if(actionType.equalsIgnoreCase("StatusBar") && data.length == 6)
+			{
+				EnviroDataTracker tracker = EM_StatusManager.lookupTracker(target);
+				float value = 0.0F;
+				
+				try
+				{
+					value = Float.parseFloat(data[5].trim());
+				} catch(NumberFormatException e)
+				{
+					return;
+				}
+				
+				if(data[4].trim().equalsIgnoreCase("santity"))
+				{
+					tracker.sanity = value;
+				} else if(data[4].trim().equalsIgnoreCase("hydration"))
+				{
+					tracker.hydration = value;
+				} else if(data[4].trim().equalsIgnoreCase("air"))
+				{
+					tracker.airQuality = value;
+				} else if(data[4].trim().equalsIgnoreCase("temp"))
+				{
+					tracker.bodyTemp = value;
+				}
+			} else if(actionType.equalsIgnoreCase("Effect") && data.length == 7)
+			{
+				if(target.worldObj.isRemote)
+				{
+					return;
+				}
+				
+				int pID = 0;
+				int dur = 0;
+				int amp = 0;
+				
+				try
+				{
+					pID = Integer.parseInt(data[4].trim());
+					dur = Integer.parseInt(data[5].trim());
+					amp = Integer.parseInt(data[6].trim());
+				} catch(NumberFormatException e)
+				{
+					return;
+				}
+				
+				if(pID >= Potion.potionTypes.length || pID < 0)
+				{
+					return;
+				}
+				
+				Potion potion = Potion.potionTypes[pID];
+				
+				target.addPotionEffect(new PotionEffect(pID, dur, amp));
+			} else if(actionType.equalsIgnoreCase("ItemShower") && data.length == 5)
+			{
+				if(target.worldObj.isRemote)
+				{
+					return;
+				}
+				
+				int iID = 0;
+				
+				try
+				{
+					iID = Integer.parseInt(data[4].trim());
+				} catch(NumberFormatException e)
+				{
+					return;
+				}
+				
+				if(iID < 0 || iID >= Item.itemsList.length)
+				{
+					return;
+				}
+				
+				for(double i = target.posX - 2; i <= target.posX + 2; i++)
+				{
+					for(double k = target.posZ - 2; k <= target.posZ + 2; k++)
+					{
+						EntityItem item = new EntityItem(target.worldObj, i, 200D, k, new ItemStack(Item.itemsList[iID]));
+						target.worldObj.spawnEntityInWorld(item);
+					}
+				}
+			} else
+			{
+				return;
+			}
+			
+			if(!message.equals(""))
+			{
+				target.sendChatToPlayer(ChatMessageComponent.createFromText(message));
 			}
 			
 			lastSurpriseID = surpriseID;
@@ -172,7 +305,7 @@ public class SurpriseHandler
 		int responseCode = con.getResponseCode();
 		if(responseCode != HttpURLConnection.HTTP_OK)
 		{
-			EnviroMine.logger.log(Level.WARNING, "Surprise request returned response code: " + responseCode);
+			EnviroMine.logger.log(Level.WARNING, "Update request returned response code: " + responseCode);
 		}
 		StringBuffer buffer = new StringBuffer();
 		int chars_read;
